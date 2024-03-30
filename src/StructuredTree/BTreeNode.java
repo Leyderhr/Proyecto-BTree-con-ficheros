f364c7163ad243227ac921ce176f458759ba716a
Leyderhr <165097459+Leyderhr@users.noreply.github.com>
2024-03-30 13:46:37 -0400

    Ya estan agregadas las clases principales.

--- src/StructuredTree/BTreeNode.java
+++ src/StructuredTree/BTreeNode.java
@@ -46,55 +46,7 @@ public class BTreeNode <E extends Comparable<E>>{
 
 
 
-    /**Metodo para buscar */
-    public int findWhere(E key) {
-        int x = -1;
 
-        if (key.compareTo(keys.get(0)) < 0)
-            x = 0;
-        else if (key.compareTo(keys.get(keys.size() - 1)) > 0)
-            x = keys.size();
-        else {
-            boolean agg = false;
-            int left = 0;
-            int right = keys.size() - 1;
-            int mid;
-            while (!agg) {
-                mid = (right + left) / 2;
-                if (key.compareTo(keys.get(mid)) > 0) {
-                    if (key.compareTo(keys.get(mid + 1)) < 0) {
-                        agg = true;
-                        x = mid + 1;
-                    }
-                    left = mid;
-                } else {
-                    if (key.compareTo(keys.get(mid - 1)) > 0) {
-                        agg = true;
-                        x = mid;
-                    }
-                    right = mid;
-                }
-            }
-        }
-        return x;
-    }
-
-    /**Metodo para agregar un key a un nodo, retorna null si el nodo esta lleno, si esta vacio
-     * o si no contiene al key que queremos ingresar*/
-    public E addKey(E key) {
-        E add = null;
-
-        if (!keys.isEmpty()) {
-            if (!keys.contains(key))
-                if (keys.size() < order - 1) {
-                    int x = findWhere(key);
-                    keys.add(x, key);
-                } else
-                    add = key;
-        } else
-            keys.add(key);
-        return  add;
-    }
 
 
 }
